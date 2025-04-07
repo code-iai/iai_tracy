4a80aa5c3097305512efa30bc5731e01ca4f3d1f
maltehue <huerkamp@uni-bremen.de>
2025-04-07 18:02:09 +0200

    configured rviz setup to ros2

--- iai_tracy_description/launch/display.launch.py
+++ iai_tracy_description/launch/display.launch.py
@@ -33,7 +33,7 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            arguments=['--display-config', rviz_file],
-            output='screen'
+            arguments=['-d', rviz_file],
+            output='log'
         )
     ])
